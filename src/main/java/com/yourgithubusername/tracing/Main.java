f484a9768f2ca35a9c18eb7ea44f450701516c60
Sourav <sourav15102@iiitd.ac.in>
2025-02-11 16:20:08 -0500

    Added(models, dependency): Added conenction and microserviceGraph models along with dependencies.

--- src/main/java/com/yourgithubusername/tracing/Main.java
+++ src/main/java/com/yourgithubusername/tracing/Main.java
@@ -1,4 +1,6 @@
 package com.yourgithubusername.tracing;
+import java.util.*;
+import java.lang.*;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/yourgithubusername/tracing/model/MicroserviceGraph.java
@@ -0,0 +1,22 @@
+package com.yourgithubusername.tracing.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MicroserviceGraph {
+    private final Map<String, List<Connection>> graph = new HashMap<>();
+
+    public void addEdge(String from, String to, int latency) {
+        this.graph.computeIfAbsent(from, k -> new ArrayList<>()).add(new Connection(to, latency));
+    }
+
+    public List<Connection> getEdges(String service) {
+        return this.graph.getOrDefault(service, new ArrayList<>());
+    }
+
+    public boolean containsNode(String service) {
+        return this.graph.containsKey(service);
+    }
+}
